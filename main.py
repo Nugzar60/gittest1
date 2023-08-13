58990ab457e6c53196d0fa4200d09895a84e614d
GiorgiyProjects <giorgiy.projects@gmail.com>
2023-08-13 12:09:59 +0300

    Удалил пустые строчки

--- main.py
+++ main.py
@@ -32,22 +32,6 @@ async def read_root():
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.

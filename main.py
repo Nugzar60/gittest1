c16a43a8c4f9835642a2ba7719de34da90dd2c9f
GiorgiyProjects <giorgiy.projects@gmail.com>
2023-08-13 12:32:56 +0300

    Почистить папин код

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
+import numpy as np
+import pandas as pd
 
 app = FastAPI()
 
@@ -9,48 +11,15 @@ async def root():
 
 @app.get("/response/")
 async def read_root():
-    a = 1
-    b = 4
     return {"response": "success3"}
 
 @app.get("/count/")
-async def read_root():
-# Определяем переменную str
-    str = "Hello, World!"
-
-# Выполняем операцию с переменной str
-    result = len(str)
-
-# Выводим результат на экран
-    print("letters:",result)
-    print("privet papa")
-    return {result,"letters:"}
-
-
-
+async def read_root(input_string: str, number: int):
+    result = len(input_string)
+    return {"letters": result, "number": number}
+    #return {result,"letters:"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(name)  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    import numpy as np
-    import pandas as pd
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-import numpy as np
-import pandas as pd
 
